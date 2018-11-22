0261d9f1fafd2892479754efb0f9d4c09291b637
Andreas Marek <andimarek@fastmail.fm>
2018-11-22 21:38:08 +1100

    cleanup

--- graphql-java-spring-webmvc/src/main/java/graphql/spring/web/servlet/components/DefaultExecutionResultHandler.java
+++ graphql-java-spring-webmvc/src/main/java/graphql/spring/web/servlet/components/DefaultExecutionResultHandler.java
@@ -2,6 +2,7 @@ package graphql.spring.web.servlet.components;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.ExecutionResult;
+import graphql.Internal;
 import graphql.spring.web.servlet.ExecutionResultHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,17 +10,14 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.CompletableFuture;
 
 @Component
+@Internal
 public class DefaultExecutionResultHandler implements ExecutionResultHandler {
 
     @Autowired
     ObjectMapper objectMapper;
 
     @Override
-    public Object handleExecutionResult(CompletableFuture<ExecutionResult> executionResultMono) {
-        return executionResultMono.thenApply(executionResult -> handleImpl(executionResult));
-    }
-
-    private Object handleImpl(ExecutionResult executionResult) {
-        return executionResult.toSpecification();
+    public Object handleExecutionResult(CompletableFuture<ExecutionResult> executionResultCF) {
+        return executionResultCF.thenApply(ExecutionResult::toSpecification);
     }
 }
