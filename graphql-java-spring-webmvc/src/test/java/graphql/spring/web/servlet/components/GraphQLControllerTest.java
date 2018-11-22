674f1eafcc919747d684b3d06c9fd03fd4920c35
Andreas Marek <andimarek@fastmail.fm>
2018-11-22 18:06:02 +1100

    testing

--- graphql-java-spring-webmvc/src/test/java/graphql/spring/web/servlet/components/GraphQLControllerTest.java
+++ graphql-java-spring-webmvc/src/test/java/graphql/spring/web/servlet/components/GraphQLControllerTest.java
@@ -1,8 +1,13 @@
 package graphql.spring.web.servlet.components;
 
+import graphql.ExecutionInput;
+import graphql.ExecutionResultImpl;
+import graphql.GraphQL;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,7 +18,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,6 +42,9 @@ public class GraphQLControllerTest {
     @Autowired
     private WebApplicationContext wac;
 
+    @Autowired
+    GraphQL graphql;
+
     @Before
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
@@ -39,12 +52,22 @@ public class GraphQLControllerTest {
 
     @Test
     public void testGetRequest() throws Exception {
-        String query = "{foo}";
-        String variablesJson = "{\"key\":\"value\"}";
+        String variablesJson = "{\"variable\":\"variableValue\"}";
+        String query = "query myQuery {foo}";
+        String operationName = "myQuery";
+
+        ExecutionResultImpl executionResult = ExecutionResultImpl.newExecutionResult()
+                .data("bar")
+                .build();
+        CompletableFuture cf = CompletableFuture.completedFuture(executionResult);
+        ArgumentCaptor<ExecutionInput> captor = ArgumentCaptor.forClass(ExecutionInput.class);
+        Mockito.when(graphql.executeAsync(captor.capture())).thenReturn(cf);
+
 
         MvcResult mvcResult = this.mockMvc.perform(get("/graphql")
                 .param("query", query)
-                .param("variables", variablesJson))
+                .param("variables", variablesJson)
+                .param("operationName", operationName))
                 .andExpect(status().isOk())
                 .andExpect(request().asyncStarted())
                 .andReturn();
@@ -52,8 +75,16 @@ public class GraphQLControllerTest {
         this.mockMvc.perform(asyncDispatch(mvcResult))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("data", is("foo")))
+                .andExpect(jsonPath("data", is("bar")))
                 .andReturn();
+
+        assertThat(captor.getAllValues().size(), is(1));
+
+        Map<String, Object> variables = new LinkedHashMap<>();
+        variables.put("variable", "variableValue");
+        assertThat(captor.getValue().getQuery(), is(query));
+        assertThat(captor.getValue().getVariables(), is(variables));
+        assertThat(captor.getValue().getOperationName(), is(operationName));
 
     }
 
