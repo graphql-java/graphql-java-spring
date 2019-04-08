790bff818ac84ad6b22ea61339ed5ed9903fe4c3
Andreas Marek <andimarek@fastmail.fm>
2019-04-08 17:33:11 +1000

    add javadoc

--- graphql-java-spring-webflux/src/main/java/graphql/spring/web/reactive/ExecutionInputCustomizer.java
+++ graphql-java-spring-webflux/src/main/java/graphql/spring/web/reactive/ExecutionInputCustomizer.java
@@ -5,6 +5,12 @@ import graphql.PublicApi;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+/**
+ * Lets you customize the #ExecutionInput before the query is executed.
+ * You can for example set a context object or define a root value.
+ * <p/>
+ * This is only used if you use the default {@link GraphQLInvocation}.
+ */
 @PublicApi
 public interface ExecutionInputCustomizer {
 
--- graphql-java-spring-webmvc/src/main/java/graphql/spring/web/servlet/ExecutionInputCustomizer.java
+++ graphql-java-spring-webmvc/src/main/java/graphql/spring/web/servlet/ExecutionInputCustomizer.java
@@ -6,6 +6,12 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Lets you customize the #ExecutionInput before the query is executed.
+ * You can for example set a context object or define a root value.
+ * <p/>
+ * This is only used if you use the default {@link GraphQLInvocation}.
+ */
 @PublicApi
 public interface ExecutionInputCustomizer {
 
