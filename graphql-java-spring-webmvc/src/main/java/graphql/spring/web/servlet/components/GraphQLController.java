87e21fa6d8af3d32159d566f61a4a75dcb81c49a
Jakub Brodzinski <jakub.brodzinski@yahoo.com>
2019-05-24 11:48:47 +0200

    First attempt to make graph POSTs with multiPartFiles work

--- graphql-java-spring-webmvc/src/main/java/graphql/spring/web/servlet/components/GraphQLController.java
+++ graphql-java-spring-webmvc/src/main/java/graphql/spring/web/servlet/components/GraphQLController.java
@@ -9,6 +9,7 @@ import graphql.spring.web.servlet.JsonSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,11 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.naming.OperationNotSupportedException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -46,7 +53,7 @@ public class GraphQLController {
             @RequestParam(value = "operationName", required = false) String operationName,
             @RequestParam(value = "variables", required = false) String variablesJson,
             @RequestBody(required = false) String body,
-            WebRequest webRequest) throws IOException {
+            WebRequest webRequest) throws IOException, OperationNotSupportedException {
 
         if (body == null) {
             body = "";
@@ -69,6 +76,45 @@ public class GraphQLController {
                 request.setQuery("");
             }
             return executeRequest(request.getQuery(), request.getOperationName(), request.getVariables(), webRequest);
+        }else if(contentType!=null && contentType.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)){
+            // In this case body is not mapped, same as all on @RequestParams.
+            // Because we send files as variables we just have to take care of variables part of deserialized GraphQLRequestBody.
+            // "map" parameter tells us how to link multiPartFiles to their variables.
+            // Map can look like this:
+            // {
+            // "0" : ["variables.files"],
+            // "1" : ["variables.filesList.0"],
+            // "2" : ["variables.filesList.1"]
+            // }
+            // It tells us that multipartFile
+            // that has a key "0" should be linked to variable named file, and multipartFile with a key "1" should be put as 0th indexed element
+            // in list named filesList in variables and "2" as a 1st element in the same list.
+            GraphQLRequestBody request = jsonSerializer.deserialize(webRequest.getParameter("operations"),GraphQLRequestBody.class);
+            if(request.getQuery() != null) {
+                LinkedHashMap<String, ArrayList<String>> multipartFileKeyVariablePathMap = this.jsonSerializer.deserialize(webRequest.getParameter("map"), LinkedHashMap.class);
+
+                StandardMultipartHttpServletRequest multiPartRequest = (StandardMultipartHttpServletRequest) ((ServletWebRequest) webRequest).getNativeRequest();
+                Map<String, MultipartFile> multipartFileMap = multiPartRequest.getFileMap();
+
+                for(Map.Entry<String,MultipartFile> e: multipartFileMap.entrySet()){
+                    String pathString = multipartFileKeyVariablePathMap.get(e.getKey()).get(0); /*i.e. "variables.files" or "variables.fileList.NUMBER*/
+                    if (pathString.matches("variables\\.[a-zA-Z0-9]*?\\.\\d")) {
+                        String[] splittedPath = pathString.split("\\.", 3);
+                        final Object variablesArray = request.getVariables().get(splittedPath[1]);
+                        if (variablesArray instanceof ArrayList)
+                            ((ArrayList) variablesArray).set(Integer.parseInt(splittedPath[2]), e.getValue());
+                        else
+                            throw new OperationNotSupportedException("Array of files represented by not supported collection");
+                    } else if (pathString.startsWith("variables.")) {
+                        String[] splittedPath = pathString.split("\\.",2);
+                        request.getVariables().put(splittedPath[1],e.getValue());
+                    }
+                }
+            }else {
+                request.setQuery("");
+            }
+
+            return executeRequest(request.getQuery(),request.getOperationName(),request.getVariables(),webRequest);
         }
 
         // In addition to the above, we recommend supporting two additional cases:
